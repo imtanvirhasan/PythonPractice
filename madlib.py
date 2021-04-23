9b5a3e4c2227ae06bd143a018a84bfad045ec9ec
Md Tanvir Hasan <th.tanvir27@gmail.com>
2021-04-24 01:30:02 +0600

    New file added

---
+++ madlib.py
@@ -0,0 +1,9 @@
+
+adj = input("Enter Adj:")
+verb = input("Enter verb:")
+famous_person = input("famous coder name:")
+
+madlib = f"Computer programming is so {adj}! It makes me excited all the time because \
+i love to {verb}. Be a coder like Mr. {famous_person}"
+
+print(madlib)
