64b05ccd7c19f62f374cfa24cfec979815fa1d25
tanvir <th.tanvir27@gmail.com>
2021-04-23 22:42:36 +0600

    New code added in this file

--- results.py
+++ results.py
@@ -1,3 +1,17 @@
 marks = int(input("Enter your marks:"))
 if marks > 100:
-    print("Invalid marks!")
+    print ("Invalid marks!")
+elif marks > 80:
+    print ("You have got A+")
+elif marks > 70:
+    print ("You have got A")
+elif marks > 60:
+    print ("You have got A-")
+elif marks > 50:
+    print ("You have got B")
+elif marks > 40:
+    print ("You have got C")
+elif marks < 0:
+    print ("Invalid marks")
+else:
+    print ("You have got F")
