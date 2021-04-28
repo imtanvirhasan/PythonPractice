33659553fde4e5de21b620758f7b0b8dfb94cddc
Md Tanvir Hasan <th.tanvir27@gmail.com>
2021-04-29 02:36:52 +0600

    loop added

--- loop.py
+++ loop.py
@@ -1,6 +1,11 @@
-total = 0
-for i in range(1, 100):
-    if i % 3 == 0 or i % 5 == 0:
-        total += i
-print (total)
+x = int(input("How many candies you need?"))
+av = 10
+i = 1 
+while i <= x:
+    if i > av:
+        print("Out of stock")
+        break
+    print("Candy")
+    i += 1
 
+print("Have a good day!")
